97e656fa4470133ceae97484fc6739138f00532e
rashed2150 <103816097+rashed2150@users.noreply.github.com>
2022-11-02 20:36:50 -0400

    first commit

---
+++ udacrew.js
@@ -0,0 +1,19 @@
+let captain = "Mal";
+let second = "Zoe";
+let pilot = "Wash";
+let companion = "Inara";
+let mercenary = "Jayne";
+let mechanic = "Kaylee";
+
+
+let crew = [captain, second, pilot, companion, mercenary, mechanic];
+
+// Another method
+// var crew = [captain, second, pilot, companion, mercenary, mechanic];
+
+// Print
+for (x in crew){
+  console.log(crew[x])
+}
+
+console.log(crew);
