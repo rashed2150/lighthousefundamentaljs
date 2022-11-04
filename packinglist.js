6423d87774a09c26b4e66073d1cf8adde34cc3e1
rashed2150 <103816097+rashed2150@users.noreply.github.com>
2022-11-04 01:59:41 -0400

    packinglist

---
+++ packinglist.js
@@ -0,0 +1,17 @@
+const packingList = ["bowls", "plates", "glasses", "cups", "cooking utensils"];
+
+/*console.log("Kitchen stuff to pack:");
+for (let i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+} */
+
+for ( let i = 0 ; i < packingList.length ; i++) {
+  console.log(packingList[i])
+}
+
+
+const shippackinglist = ["Jeans", "Chino", "underwear", "vest", "socks", "Shaving kit"]
+console.log("Remember to take following things")
+for( let x = 0 ; x < shippackinglist.length ; x++){
+  console.log(shippackinglist[x])
+}
